2e6911def8634a77253c10564588dd9f27439af3
Tomas Perez <tommii.perez@gmail.com>
2019-04-26 13:11:59 -0300

    tests

--- ModeloExamen/CasiBuscaMinas.php
+++ ModeloExamen/CasiBuscaMinas.php
@@ -1,12 +1,9 @@
 <?php
-
 class BuscaMinas {
-
   private $minas = array();
   private $perdio = false;
   private $termino = false;
   private $cantidadMinas = 0;
-
   public function __construct($ancho, $largo) {
     for($i=0; $i<$ancho; $i++) {
       for($j=0; $j<$largo; $j++) {
@@ -14,11 +11,15 @@ class BuscaMinas {
       }
     }
   }
-
   public function agregarMina($x, $y) {
+    if (!isset($this->minas[$x][$y])) {
+      return false;
+    }
+    if ($this->minas[$x][$y] < 0) {
+      return false;
+    }
     $this->minas[$x][$y] = -9999;
     $this->cantidadMinas += 1;
-
     if (isset($this->minas[$x-1][$y-1])) {
       $this->minas[$x-1][$y-1] += 1;
     }
@@ -43,32 +44,34 @@ class BuscaMinas {
     if (isset($this->minas[$x+1][$y+1])) {
       $this->minas[$x+1][$y+1] += 1;
     }
+    return true;
   }
-
   public function jugar($x, $y) {
-    if ($this->minas[$x][$y]>0) {
-      return $this->minas[$x][$y];
+    if ($this->minas[$x][$y]<0) {
+      $this->perdio = true;
+      $this->termino = true;
+      return false;
     }
-    $this->perdio = true;
-    return false;
+    return $this->minas[$x][$y];
   }
-
   public function terminoDeJugar() {
     return $this->perdio || $this->termino;
   }
-
   public function gano() {
     return !$this->perdio && $this->termino;
   }
-
   public function sacarMina($x, $y) {
+    if ($this->minas[$x][$y] >= 0) {
+      $this->termino = true;
+      $this->perdio = true;
+      return false;
+    }
     $this->minas[$x][$y] = 0;
     $this->cantidadMinas -= 1;
     if ($this->cantidadMinas == 0) {
       $this->termino = true;
       $this->perdio = false;
     }
-
     if (isset($this->minas[$x-1][$y-1])) {
       $this->minas[$x-1][$y-1] += 1;
     }
@@ -93,5 +96,6 @@ class BuscaMinas {
     if (isset($this->minas[$x+1][$y+1])) {
       $this->minas[$x+1][$y+1] += 1;
     }
+    return true;
   }
 }
--- ModeloExamen/TestBuscaMinas.php
+++ ModeloExamen/TestBuscaMinas.php
@@ -1,4 +1,60 @@
 <?php
 
 require_once('./vendor/autoload.php');
-require('./BilleteraInterface.php');
+require('./CasiBuscaMinas.php');
+
+use PHPUnit\Framework\TestCase;
+
+final class TestBuscaMinas extends TestCase 
+{
+    public function crearTablero ()
+    {
+        $tablero= new BuscaMinas (8,8);
+        $tablero->agregarMina (2,3);
+        $tablero->agregarMina (1,7);
+        $tablero->agregarMina (5,3);
+        return $tablero;
+    }
+    public function testExistsBuscaMinas() {
+        $this->assertTrue(class_exists("BuscaMinas"));
+      }
+    public function testJugar()
+    {
+        $tablero = $this->crearTablero();
+        $tablero->jugar(1,3);
+        $tablero->jugar(2,5);
+        $tablero->jugar(7,7);
+        $tablero->jugar(5,5);
+        $tablero->jugar(4,2);
+        $this->assertFalse($tablero->gano());
+    }
+    public function testPerdio()
+    {
+        $tablero = $this->crearTablero();
+        $this->assertFalse($tablero->jugar(1,7));
+    }
+    public function testAgregarFuera()
+    {
+        $tablero = $this->crearTablero();
+        $this->assertFalse ($tablero->agregarMina(9,9));
+    }
+    public function testAgregoMina()
+    {
+        $tablero = $this->crearTablero();
+        $this->assertTrue($tablero->agregarMina (5,5));
+        $this->assertTrue($tablero->agregarMina (7,7));
+    }
+
+    public function testGano()
+    {
+        $tablero = $this->crearTablero();   
+        $tablero->sacarMina(2,3);
+        $tablero->sacarMina(1,7);
+        $tablero->sacarMina(5,3);
+        $this->assertTrue($tablero->gano());
+    }
+    public function testMinaDuplicada (){
+        $tablero = $this->crearTablero();
+        $this->assertFalse ($tablero->agregarMina(2,3));
+    }
+}
