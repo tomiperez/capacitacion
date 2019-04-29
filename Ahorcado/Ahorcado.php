3f261efb041f4c6aac0766ac798e20c601a4e537
Tomas Perez <tommii.perez@gmail.com>
2019-04-29 16:51:07 -0300

    ahorcado

--- Ahorcado/Ahorcado.php
+++ Ahorcado/Ahorcado.php
@@ -1,24 +1,86 @@
 <?php
-
 require './vendor/autoload.php';
-
 class Ahorcado {
   private $palabra;
   private $intentos;
-
+  private $resultado;
   public function __construct($palabra, $intentos) {
     $this->palabra = $palabra;
     $this->intentos = $intentos;
   }
+  public function armarTablero(){
+    for ($i=0; $i< strlen($this->palabra); $i++){
+      $this->resultado[$i] = "_ ";
+      }
+      
+  }
   public function damePalabra() {
     return $this->palabra;
   }
-
   public function dameIntentos() {
     return $this->intentos;
   }
-
-  public function mostrarTablero() {
-    return "_ _ _ _ _";
+  public function mostrarResultado() {
+    for ($i=0; $i< strlen($this->palabra); $i++){
+      echo $this->resultado[$i] . '';
+    }
+  }
+  public function pasarLetra($caracter)
+  {
+    if ($this->intentos>0){
+      $fallos=0;
+      $tempL=strlen ($this->palabra);
+      for ($i=0; $i<$tempL; $i++){
+        if ($this->palabra[$i]===$caracter && $this->resultado[$i] != $caracter){
+          $this->resultado[$i]= $caracter;
+        }else{
+          $fallos++;
+        }
+      }
+      if ($fallos===$tempL){
+        $this->intentos--;
+        return false;
+      }
+      return true;  
+    }
+  }
+   
+  
+ public function dameIntentosRestantes()
+  {
+    return $this->intentos;
+  }
+  public function perdio()
+  {
+    if($this->intentos == 0){
+      return true;
+    }
+    return false;
+  }
+  public function gano(){
+    $arrayTemp = array_diff(str_split($this->palabra), $this->resultado);
+    if(sizeof($arrayTemp) == 0){
+      echo 'Ganó!!' . "\n";
+      return true;
+    }else{
+      return false;
+    }
   }
 }
+$ahor = new Ahorcado('chachara', 9);
+$ahor->armarTablero();
+$ahor->pasarLetra('h');
+$ahor->mostrarResultado();
+echo "\n";
+$ahor->gano();
+$ahor->pasarLetra('c');
+$ahor->mostrarResultado();
+echo "\n";
+$ahor->gano();
+$ahor->pasarLetra('a');
+$ahor->mostrarResultado();
+echo "\n";
+$ahor->pasarLetra('r');
+$ahor->mostrarResultado();
+echo "\n";
+$ahor->gano();
--- Ahorcado/AhorcadoTest.php
+++ Ahorcado/AhorcadoTest.php
@@ -1,19 +1,15 @@
 <?php
 require_once('./vendor/autoload.php');
 require('./Ahorcado.php');
-
 use PHPUnit\Framework\TestCase;
-
 final class AhorcadoTest extends TestCase
 {
   public function testExisteAhorcado() {
     $this->assertTrue(class_exists("Ahorcado"));
   }
-
   public function testPalabraEnConstructor() {
     $ahorcado = new Ahorcado("chachara", 5);
   }
-
   public function testTieneLaPalabraQueYoDigo() {
     $palabra = "chachara".rand(0, 1000);
     $ahorcado = new Ahorcado($palabra, 5);
@@ -21,25 +17,22 @@ final class AhorcadoTest extends TestCase
       $palabra,
       $ahorcado->damePalabra());
   }
-
   public function testTieneIntentos() {
     $intentos = rand(0, 500);
     $ahorcado = new Ahorcado("aeou", $intentos);
     $this->assertEquals($intentos, $ahorcado->dameIntentos());
   }
-
   public function testEstaLaLetra() {
     $ahorcado = new Ahorcado("chachara", 5);
+    $ahorcado->mostrarResultado();
     $esta = $ahorcado->pasarLetra('h');
     $this->assertEquals(True, $esta, "Esta la h");
   }
-
   public function testNoEstaLetra() {
     $ahorcado = new Ahorcado("chachara", 5);
     $esta = $ahorcado->pasarLetra('e');
     $this->assertEquals(False, $esta, "No esta");
   }
-
   public function testRestaIntentos() {
     $ahorcado = new Ahorcado("chachara", 5);
     $this->assertEquals(
@@ -48,12 +41,10 @@ final class AhorcadoTest extends TestCase
     $ahorcado->pasarLetra('z');
     $this->assertEquals(
       4,$ahorcado->dameIntentosRestantes());
-
     $ahorcado->pasarLetra('h');
     $this->assertEquals(
       4,$ahorcado->dameIntentosRestantes());
   }
-
   public function testSiEsBoludoResta() {
     $ahorcado = new Ahorcado("chachara", 5);
     $this->assertEquals(
@@ -62,12 +53,10 @@ final class AhorcadoTest extends TestCase
     $ahorcado->pasarLetra('h');
     $this->assertEquals(
       5,$ahorcado->dameIntentosRestantes());
-
     $ahorcado->pasarLetra('h');
     $this->assertEquals(
       4,$ahorcado->dameIntentosRestantes());
   }
-
   function testMostrarTodoOculto() {
     $ahorcado = new Ahorcado("cha", 5);
     $this->assertEquals(
@@ -75,7 +64,6 @@ final class AhorcadoTest extends TestCase
       $ahorcado->mostrar()
     );
   }
-
   function testMostrarCasiTodoOculto() {
     $ahorcado = new Ahorcado("cha", 5);
     $ahorcado->pasarLetra("h");
@@ -84,14 +72,12 @@ final class AhorcadoTest extends TestCase
       $ahorcado->mostrar()
     );
     $this->assertFalse($ahorcado->gano());
-
     $ahorcado->pasarLetra("a");
     $this->assertEquals(
       '_ h a',
       $ahorcado->mostrar()
     );
     $this->assertFalse($ahorcado->gano());
-
     $ahorcado->pasarLetra("c");
     $this->assertEquals(
       'c h a',
@@ -99,18 +85,15 @@ final class AhorcadoTest extends TestCase
     );
     $this->assertTrue($ahorcado->gano());
   }
-
   function testGano() {
     $ahorcado = new Ahorcado("ab", 5);
     $ahorcado->pasarLetra("a");
     $this->assertFalse($ahorcado->perdio());
     $this->assertFalse($ahorcado->gano());
-
     $ahorcado->pasarLetra("b");
     $this->assertFalse($ahorcado->perdio());
     $this->assertTrue($ahorcado->gano());
   }
-
   function testPerdio() {
     $ahorcado = new Ahorcado("a", 1);
     $ahorcado->pasarLetra("z");
@@ -118,5 +101,7 @@ final class AhorcadoTest extends TestCase
     $this->assertFalse($ahorcado->gano());
     $this->assertEquals(0, $ahorcado->dameIntentosRestantes());
   }
-
 }
+
+// linea para ejecutar el test
+// ./vendor/bin/phpunit AhorcadoTest.php
